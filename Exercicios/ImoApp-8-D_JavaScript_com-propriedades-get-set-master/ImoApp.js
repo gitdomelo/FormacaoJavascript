c67686854df97d1e1e01f7bb0a0e36bd8e533d8d
gitdomelo <correiodomiguelmelo@gmail.com>
2020-09-24 09:50:41 +0100

    more loose v2

--- Exercicios/ImoApp-8-D_JavaScript_com-propriedades-get-set-master/ImoApp.js
+++ Exercicios/ImoApp-8-D_JavaScript_com-propriedades-get-set-master/ImoApp.js
@@ -18,28 +18,7 @@ class ImoApp {
 		this.clientes.registar(new Cliente(rua, número, cidade, códigoPostalPrincipal, códigoPostalsecundário, nome, email, telemóvel));
 	}
 
-	mostrarImóveis(imoveis,tableHtml)
-	{
-		var table_mostrar = document.getElementById(tableHtml);
-		//limpar tabela
 	
-		while(table_mostrar.rows.length > 1) {
-			table_mostrar.deleteRow(1);	
-		}
-        //preencherTabela
-		imoveis.forEach(element => {
-			var row= table_mostrar.insertRow(1);
-			var cell1 = row.insertCell(0);
-			var cell2 = row.insertCell(1);
-			var cell3 = row.insertCell(2);
-			var cell4 = row.insertCell(3);
-			var cell5 = row.insertCell(4);
-			cell1.innerHTML = element.endereço.rua;
-			cell2.innerHTML = element.endereço.número;
-			cell3.innerHTML = element.endereço.cidade;
-			cell4.innerHTML = element.endereço.freguesia.nome;
-			cell5.innerHTML = element.valor;
-		});
 
 
 	}
